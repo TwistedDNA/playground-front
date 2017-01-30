2d8f2ff685ba4ff6a4b85ca1867e660f908d1b43
mmazur <maksym_mazur@epam.com>
2017-01-30 14:35:18 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileFormComponent
   ],
   imports: [
     BrowserModule,
